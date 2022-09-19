044e8d9a3041624979055e1be9c9401fb72bd3fa
Daniel Silva <daniel1uno@gmail.com>
2022-09-19 08:41:06 -0500

    fixed out path bug

--- anonymizer.py
+++ anonymizer.py
@@ -8,22 +8,25 @@ from datetime import datetime
 print('Started at '+ str(datetime.now()))
 
 # paths to custom yolov4 model
-pth_weights = r'YoloV4_custom_weights\yolov4-custom_best.weights'
-pth_cfg = r'YoloV4_custom_weights\yolov4-custom.cfg'
+pth_weights = r"YoloV4_custom_weights\yolov4-custom_best.weights"
+pth_cfg = r"YoloV4_custom_weights\yolov4-custom.cfg"
 
 model = cv2.dnn.readNet(pth_weights, pth_cfg)  # read the model here
 
-# example: data/ <-- dont forget the /
-in_path = input(r'Ruta de entrada de las imagenes: ')
-# example: results/ <-- dont forget the /
-out_path = input(r'Ruta de salida de las imagenes: ')
+# example: data
+in_path = input(r"Ruta de entrada de las imagenes :")
+in_folder = in_path.split('\\')[-1]
 
+# example: results
+out_path = input(r"Ruta de salida de las imagenes :")
 # this looks fot jpg images, adjust as needed.
 images_path = glob.glob(in_path + '/**/*.jpg', recursive=True)
 create_logger()
 
 for image in images_path:
-    images_out_path = out_path+ntpath.split(image)[0]+"/"
+    subfolder = image.split(in_folder)[-1]
+    subfolder = ntpath.dirname(subfolder)
+    images_out_path = out_path+subfolder+"\\"
     Path(images_out_path).mkdir(parents=True, exist_ok=True)
     blur_faces_licenses(image, images_out_path, model)
 
